9eb5e849a5ab0f1c3a0d6e85b6651e28691b5cfe
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-31 19:31:18 +0530

    framer-motion setup

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,17 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion'
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        animate={{opacity: 1, y: 0}}
+        initial={{opacity: 0, y: 30}}
+        open className="modal">
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
