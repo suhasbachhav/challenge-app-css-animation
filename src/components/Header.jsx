7d5820f01aee86c85f787da00762a5c8aa0ccc31
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-31 19:40:29 +0530

    modal updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import NewChallenge from './NewChallenge.jsx';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -12,16 +13,22 @@ export default function Header() {
   function handleDone() {
     setIsCreatingNewChallenge(false);
   }
+  
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button 
+          whileHover={{scale: 1.2}}
+          transition={{type:'spring', stiffness: 500}}
+          onClick={handleStartAddNewChallenge} className="button">
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,8 +6,13 @@ export default function Modal({ title, children, onClose }) {
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        animate={{opacity: 1, y: 0}}
-        initial={{opacity: 0, y: 30}}
+        variants={{
+          hidden: {opacity: 0, y: 30},
+          visible: {opacity: 1, y: 0}
+        }}
+        animate="visible"
+        initial="hidden"
+        exit="hidden"
         open className="modal">
         <h2>{title}</h2>
         {children}
